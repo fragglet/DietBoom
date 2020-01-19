fragglet/DietBoom

commit 4375bc198bb48f971b3a23623b8888da7c99c86c

Hide coordinates in automap unless cheating.
These can be a bit distracting, and can reveal information in non-follow
mode (possibly the motivations for the map_point_coordinates option).
So hide the coordinates display unless the IDDT cheat is used.

Source/hu_stuff.c
@@ -485,12 +485,13 @@ void HU_Start(void)
 //
 void HU_Drawer(void)
 {
+  extern int ddt_cheating;
   char *s;
   player_t *plr;
 
   plr = &players[displayplayer];         // killough 3/7/98
   // draw the automap widgets if automap is displayed
-  if (automapactive)
+  if (automapactive && ddt_cheating)
     {
       fixed_t x,y,z;   // killough 10/98:
       void AM_Coordinates(const mobj_t *, fixed_t *, fixed_t *, fixed_t *);

commit d016efa632809ff601c69414e1b7852f6f8c541b

Restore map title.
I accidentally broke this in 4375bc198bb48f9.

Source/hu_stuff.c
@@ -493,11 +493,14 @@ void HU_Drawer(void)
   // draw the automap widgets if automap is displayed
   if (automapactive && ddt_cheating)
     {
-      fixed_t x,y,z;   // killough 10/98:
-      void AM_Coordinates(const mobj_t *, fixed_t *, fixed_t *, fixed_t *);
-
       // map title
       HUlib_drawTextLine(&w_title, false);
+    }
+
+  if (automapactive && ddt_cheating)
+    {
+      fixed_t x,y,z;   // killough 10/98:
+      void AM_Coordinates(const mobj_t *, fixed_t *, fixed_t *, fixed_t *);
 
       // killough 10/98: allow coordinates to display non-following pointer 
       AM_Coordinates(plr->mo, &x, &y, &z);
995cbd68a2ea119db519555e63b1fc48a2dd24c0
Simon Howard <fraggle@soulsphere.org>
2020-03-11 00:45:58 -0400

    Adjust for lack of STCFN096.
    This was removed upstream and now causes an error on startup due to
    the missing lump.

--- Source/hu_stuff.c
+++ Source/hu_stuff.c
@@ -227,7 +227,7 @@ void HU_Init(void)
   j = HU_FONTSTART;
   for (i=0;i<HU_FONTSIZE;i++,j++)
     {
-      if (j<97)
+      if (j<96)
         {
           sprintf(buffer, "STCFN%.3d",j);
           hu_font[i] = (patch_t *) W_CacheLumpName(buffer, PU_STATIC);
